78ffc72f6b1e8f35fc1c8d0f991946502af3f037
nancybalogh <131818498+nancybalogh@users.noreply.github.com>
2023-08-22 14:04:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ streamlit.dataframe(my_data_rows)
 # Allow end user to add a fruit to the list
 # add_my_fruit = my_cur.execute("insert into fruit_load_list values ('add_my_fruit')")
 
-my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
+# my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
 
 
 
